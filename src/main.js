61b77c4163f5fd486dfba09daaef6e9b085e23b6
Kongmooner <124510435+Kongmooner@users.noreply.github.com>
2023-02-17 10:00:24 +0000

    indent(1)

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vueti
+import vuetify from './plugins/vuetify';
+
+Vue.confi
